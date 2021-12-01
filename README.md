28b2875f4bf4d15ce8c4adaf45291519cec4a164
byksy <ayisan1096@gmail.com>
2021-12-01 23:37:56 +0300

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Go Learning
 It is my learning program for Go language
+
+## Shot Declaration For Go
+```
+package main 
+func main() {
+    //var width, height =100,50 //DONT!
+    width, height := 100,50 // it is ok!
+
+    //DONT!
+    //width =50 // assigns 50 to width
+    // color := "red" // new variable: color
+
+    width, color :=50, "red" //same as above, but it is so good!
+
+}
+```
--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 	"path"
 )
 
-func main() {
-	// fmt.Println(mascot.BestMascot())
-	// fmt.Println(quote.Go())
+//version:=0 DON'T DO
+//var version string You can use that
 
+func main() {
+
+	//score := 0 DONT'
+	//var score int //already score=0
 	var dir, file string
 
 	dir, file = path.Split("css/main.css")
-	fmt.Println("dir:", dir)
+
 	fmt.Println("file:", file)
+	fmt.Println("dir:", dir)
+
+	/*If you want discarding dir info you can use discard character*/
+	// var file string
+	// _ , file = path.Split("css/main.css")
+	// fmt.Println ("file:",file)
+
+	//// Grouping declaration
+	// var (
+	// 	//related
+	// 	video string
+
+	// 	//closely related
+	// 	duration int
+	// 	current int
+	// )
+
 }
