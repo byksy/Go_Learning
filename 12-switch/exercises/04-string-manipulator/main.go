74c9f09a5092ec34f433ed60602dd03408d81259
byksy <ayisan1096@gmail.com>
2021-12-22 08:35:59 +0300

    Add Switch Exercises

---
+++ 12-switch/exercises/04-string-manipulator/main.go
@@ -0,0 +1,72 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// STORY
+//  You want to write a program that will manipulate a
+//  given string to uppercase, lowercase, and title case.
+//
+// EXERCISE: String Manipulator
+//
+//  1. Get the operation as the first argument.
+//
+//  2. Get the string to be manipulated as the 2nd argument.
+//
+// HINT
+//  You can find the manipulation functions in the strings
+//  package Go documentation (ToLower, ToUpper, Title).
+//
+// EXPECTED OUTPUT
+//
+//  go run main.go
+//    [command] [string]
+//
+//    Available commands: lower, upper and title
+//
+//  go run main.go lower 'OMG!'
+//    omg!
+//
+//  go run main.go upper 'omg!'
+//    OMG!
+//
+//  go run main.go title "mr. charles darwin"
+//    Mr. Charles Darwin
+//
+//  go run main.go genius "mr. charles darwin"
+//    Unknown command: "genius"
+// ---------------------------------------------------------
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("[command][string]")
+		fmt.Println("Available commands: lower, upper, and title.")
+		return
+	}
+
+	c, s := os.Args[1], os.Args[2]
+
+	switch c {
+	case "lower":
+		fmt.Println(strings.ToLower(s))
+	case "upper":
+		fmt.Println(strings.ToUpper(s))
+	case "title":
+		fmt.Println(strings.Title(s))
+	default:
+		fmt.Printf("Unknown command: %q", c)
+	}
+}
--- Go_Learning/main.go
+++ Go_Learning/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"time"
 )
 
 func main() {
@@ -26,15 +25,39 @@ func main() {
 
 	// AUTHOR SOLUTION
 
-	arg := os.Args[1]
+	// arg := os.Args[1]
 
-	feet, err := strconv.ParseFloat(arg, 64)
-	if err != nil {
-		fmt.Printf("error: %q is not a number. \n", arg)
-		return
+	// feet, err := strconv.ParseFloat(arg, 64)
+	// if err != nil {
+	// 	fmt.Printf("error: %q is not a number. \n", arg)
+	// 	return
+	// }
+	// meters := feet * 0.3048
+
+	// fmt.Printf("%g feet is %g metrs.\n", feet, meters)
+
+	// switch t := time.Now().Hour(); {
+
+	// case t > 6 && t < 12:
+	// 	fmt.Println("Good morning.")
+	// case t >= 12 && t < 18:
+	// 	fmt.Println("Good afternoon.")
+	// case t >= 18 && t < 23:
+	// 	fmt.Println("Good evening.")
+	// default:
+	// 	fmt.Println("Good night.")
+
+	// }
+
+	switch h := time.Now().Hour(); {
+	case h >= 18:
+		fmt.Println("Good evening")
+	case h >= 12:
+		fmt.Println("Good afternoon")
+	case h >= 6:
+		fmt.Println("Good morning")
+	default:
+		fmt.Println("Good night")
 	}
-	meters := feet * 0.3048
-
-	fmt.Printf("%g feet is %g metrs.\n", feet, meters)
 
 }
