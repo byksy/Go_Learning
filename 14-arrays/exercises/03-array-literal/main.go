29c96f180fc5ec6b91962906c9e94c450a07ea59
byksy <ayisan1096@gmail.com>
2022-01-11 08:01:56 +0300

    Add more array exercises

---
+++ 14-arrays/exercises/03-array-literal/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	names := [3]string{"Mustafa", "Asya", "Nevra"}
+
+	separator := "\n" + strings.Repeat("=", 20) + "\n"
+
+	fmt.Print("names", separator)
+
+	for i := range names {
+
+		fmt.Printf("names[%d]: %q\n", i, names[i])
+	}
+
+	fmt.Print("\ndistances", separator)
+
+	distances := [5]int{50, 50, 30, 20, 10}
+
+	for i := range distances {
+
+		fmt.Printf("distances[%d]: %d\n", i, distances[i])
+	}
+
+	fmt.Print("\ndata", separator)
+
+	data := [5]uint8{1, 2, 3, 4, 5}
+
+	for i := range data {
+
+		fmt.Printf("data[%d]: %d\n", i, data[i])
+	}
+
+	for i, v := range data {
+		// try the %c verb
+		fmt.Printf("data[%d]: %c\n", i, v)
+	}
+	fmt.Print("\nratios", separator)
+
+	ratios := [1]float64{3.14}
+
+	fmt.Printf("ratios[0]: %.2f\n", ratios[0])
+
+	fmt.Print("\nalives", separator)
+
+	alives := [5]bool{true, false, true, false}
+
+	for i := range alives {
+
+		fmt.Printf("alives[%d], %t\n", i, alives[i])
+	}
+
+	fmt.Print("\nzero", separator)
+	// A byte array that doesn't occupy memory space
+	//
+	// Don't do this:
+	//   zero := [0]byte{}
+	//
+	// Do this (when you don't assign elements):
+	var zero [0]byte
+	fmt.Printf("zero: %d\n", zero)
+}
